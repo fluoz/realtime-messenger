ba240347492da5173f2273a87093d43ecc27e635
fluoz <hmunshi12@gmail.com>
2023-06-08 11:54:05 +0700

    add layout, users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,11 @@
+import EmptyState from "@/components/EmptyState";
+
 const Users = () => {
-  return <div className="hidden lg:block lg:pl-80 h-full"></div>;
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+  );
 };
 
 export default Users;
